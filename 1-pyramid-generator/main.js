4dc78f6692013b89fd63effcdfbc9d21d37f9b6c
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-05-20 02:15:46 +0300

    Add profession and age variable declarations with console logging

--- 1-pyramid-generator/main.js
+++ 1-pyramid-generator/main.js
@@ -1,1 +1,9 @@
 let character = "Hello";
+
+// Use the let keyword to declare a profession variable and an age variable. Initialize profession with the string "teacher", but do not initialize age with any value.
+let profession = "teacher";
+let age;
+
+// Log both of your variables to the console to see the results.
+console.log(profession);
+console.log(age);

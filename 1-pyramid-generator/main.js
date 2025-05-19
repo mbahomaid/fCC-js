47012083080daa9de8b37ed88cfe0480cc8614ca
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-05-20 01:58:38 +0300

    Add console log to display updated character variable value

--- 1-pyramid-generator/main.js
+++ 1-pyramid-generator/main.js
@@ -6,3 +6,6 @@ console.log(character);
 
 // After your console.log, assign the value "World" to your character variable.
 character = "World";
+
+// Now log your character variable to the console again.
+console.log(character);

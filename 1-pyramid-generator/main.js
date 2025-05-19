bcbad67f24116c0d2e06e2fff81758684227b07b
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-05-20 01:57:42 +0300

    Update character variable value to "World"

--- 1-pyramid-generator/main.js
+++ 1-pyramid-generator/main.js
@@ -3,3 +3,6 @@ let character = 'Hello';
 
 // Print the value of the character variable to the console.
 console.log(character);
+
+// After your console.log, assign the value "World" to your character variable.
+character = "World";

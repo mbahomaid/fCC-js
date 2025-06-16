2b624c015cb7ea9f83200ff06a7266813366f249
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-06-16 17:03:07 +0300

    Remove the cities array

--- 1-pyramid-generator/main.js
+++ 1-pyramid-generator/main.js
@@ -1,9 +1,5 @@
 let character = 'Hello';
 let count = 8;
-let rows = ['Naomi', 'Quincy', 'CamperChan'];
-let cities = ['London', 'New York', 'Mumbai'];
+let rows = ["Naomi", "Quincy", "CamperChan"];
 
-console.log(cities);
-cities[cities.length - 1] = 'Mexico City';
-console.log(cities);
 console.log(rows);

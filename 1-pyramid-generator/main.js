c623bb17bd24dae1cecd07e97760997cb94ebac6
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-06-16 16:57:29 +0300

    Remove commented code and clean up array logging

--- 1-pyramid-generator/main.js
+++ 1-pyramid-generator/main.js
@@ -1,11 +1,5 @@
 let character = 'Hello';
 let count = 8;
-
-// Add values to the array
 let rows = ["Naomi", "Quincy", "CamperChan"];
 
-// Use .length to access the last element in the array
-rows[rows.length - 1] = 10;
-
-// Print the first value of the array
-console.log(rows)
+console.log(rows);

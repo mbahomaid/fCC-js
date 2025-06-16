dacc0e7b0ef91f16e4f9626715ea6ddfa200f433
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-06-16 04:00:50 +0300

    Decalre an empty array

--- 1-pyramid-generator/main.js
+++ 1-pyramid-generator/main.js
@@ -1,16 +1,5 @@
-let character = "Hello";
-
-// Use let to declare a count variable. Assign it the number 8.
+let character = 'Hello';
 let count = 8;
 
-// Try printing count + 1 to the console.
-console.log(count + 1);
-
-// Print count - 1 to the console.
-console.log(count - 1);
-
-// Print count * 2 to the console.
-console.log(count * 2);
-
-// Print count / 2 to the console.
-console.log(count / 2);
+// Decalre an empty array
+let rows = [];

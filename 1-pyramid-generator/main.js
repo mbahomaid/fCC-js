5a5d4604fb3e3770e7d26f0f3d4834933beb8376
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-06-16 16:49:25 +0300

    Use .length to access the last element in the array

--- 1-pyramid-generator/main.js
+++ 1-pyramid-generator/main.js
@@ -4,8 +4,8 @@ let count = 8;
 // Add values to the array
 let rows = ["Naomi", "Quincy", "CamperChan"];
 
-// Update the value of item 3 in the arrary
-rows[2] = 10;
+// Use .length to access the last element in the array
+rows[rows.length - 1] = 10;
 
 // Print the first value of the array
 console.log(rows)

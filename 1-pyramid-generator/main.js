4975ba73c9321e3970e67dc8f946828d54e3130a
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-06-16 16:45:16 +0300

    Update the value of item 3 in the arrary

--- 1-pyramid-generator/main.js
+++ 1-pyramid-generator/main.js
@@ -4,5 +4,8 @@ let count = 8;
 // Add values to the array
 let rows = ["Naomi", "Quincy", "CamperChan"];
 
+// Update the value of item 3 in the arrary
+rows[2] = 10;
+
 // Print the first value of the array
-console.log(rows[0]);
+console.log(rows)
